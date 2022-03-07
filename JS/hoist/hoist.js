f6aa10facabe36260b1b2bad995e9e44983a71bc
Maxwell Pyle <maxwell.r.pyle@gmail.com>
2022-03-07 15:40:29 -0500

    commit

---
+++ JS/hoist/hoist.js
@@ -0,0 +1,18 @@
+// console.log(hello);
+// var hello = 'hello';
+// var hello
+// console.log(hello)
+// hello = 'hello';
+
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// var needle = 'haystack'
+// fuction test()
+// var needle = 'magnet
+// console.log(needle)'
+// test();
