c040fa531b25e9f69a8eb4547ed9aa1f447e52bb
Maxwell Pyle <maxwell.r.pyle@gmail.com>
2022-03-13 22:08:23 -0400

    commit

---
+++ React/moreforms/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UserForm from './components/UserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome! please submit your information</h1>
+      <UserForm></UserForm>
+    </div>
+  );
+}
+
+export default App;
